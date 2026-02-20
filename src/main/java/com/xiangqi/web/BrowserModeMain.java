279320f685eedf9ef91cbbfbd87217433760ae20
Zzy-min <Zzy-min@users.noreply.github.com>
2026-02-20 09:48:01 +0800

    fix(web): use 127.0.0.1 as default browser-mode trusted host

--- src/main/java/com/xiangqi/web/BrowserModeMain.java
+++ src/main/java/com/xiangqi/web/BrowserModeMain.java
@@ -5,7 +5,7 @@ import java.util.concurrent.CountDownLatch;
 
 public final class BrowserModeMain {
     public static final int PORT = 18388;
-    public static final String TRUSTED_HOST = "xiangqi.localhost";
+    public static final String TRUSTED_HOST = "127.0.0.1";
 
     private BrowserModeMain() {
     }
